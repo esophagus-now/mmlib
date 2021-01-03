b484890eb0e00973793e889620cb285c93b51657
Marco <mahkoe@gmail.com>
2021-01-03 13:07:39 -0500

    Drafted in headers

---
+++ graph.h
@@ -0,0 +1,133 @@
+#ifdef MM_IMPLEMENT
+	#ifndef GRAPH_H_IMPLEMENTED
+		#define GRAPH_H_IMPLEMENTED 1
+		#define SHOULD_INCLUDE 1
+	#else
+		#define SHOULD_INCLUDE 0
+	#endif
+#else
+	#ifndef GRAPH_H
+		#define GRAPH_H 1
+		#define SHOULD_INCLUDE 1
+	#else
+		#define SHOULD_INCLUDE 0
+	#endif
+#endif
+
+#if SHOULD_INCLUDE
+
+#ifdef MM_IMPLEMENT
+#undef MM_IMPLEMENT
+#include "graph.h"
+#define MM_IMPLEMENT 1
+#endif
+
+#include <assert.h>
+#include <stdlib.h>
+
+//Only works with square matrices.
+void adj_mat_mul(char *dest, char const *srcA, char const *srcB, int n) 
+#ifdef MM_IMPLEMENT
+{
+	//Dest and srcs may not alias, but srcs may alias
+	assert(((dest + n*n) <= srcA) || ((srcA + n*n) <= dest));
+	assert(((dest + n*n) <= srcB) || ((srcB + n*n) <= dest));
+	
+	int i, j, k;
+	for (i = 0; i < n; i++) {
+		char *dest_row = dest + i*n;
+		for (j = 0; j < n; j++) {
+			char res = 0;
+			for (k = 0; k < n; k++) {
+				if (srcA[i*n + k] && srcB[k*n + j]) {
+					res = 1;
+					break;
+				}
+			}
+			dest_row[j] = res;
+		}
+	}
+}
+#else
+;
+#endif
+
+
+// Technique: transitive closure of an adjacency A of size n by n is equal to:
+//   A^n + A^(n-1) + ... + A
+// where multiplication (between elements of A) is AND and and addition (between
+// elements of A) is OR. In the above expression, A^n means repeated matrix products.
+//
+// It turns out this is equal to:
+//  (A+I)^(n-1) * A
+//
+// This works because OR is idempotent. If you do the binomial expansion of 
+// (A+I)^(n-1), the (scalar) multiplication in front of terms is actually 
+// repeated OR'ing.
+//
+// One more thing. A graph-theoretical argument (that I won't prove here, mostly
+// because I don't know how to prove it) shows that actually we can do
+//
+//  (A+I)^M * A, where M >= (n-1)
+//
+// Intuititively, this is because this product still find all the paths of length
+// up to N, which is all that is needed to compute transitive closure. The reason
+// to raise the matrix to a higher power is because we can just use repeated squaring
+// and pick M to be the largest power of 2 that is greater than or equal to (n-1).
+//
+void transitive_closure(char *dest, char const *src, int n) 
+#ifdef MM_IMPLEMENT
+{
+	//Matrices may not alias
+	//TODO: it should be possible to get around this without sacrificing 
+	//performance, because I had to copy src anyway to add the identity
+	assert(((dest + n*n) <= src) || ((src + n*n) <= dest));
+	
+	//Corner cases
+	assert(n > 0);
+	if (n == 1) {
+		*dest = *src;
+		return;
+	}
+	
+	char *tmp = malloc(n*n);
+	if (!tmp) FAST_FAIL("out of memory");
+	
+	char *tmp2 = malloc(n*n);
+	if (!tmp2) FAST_FAIL("out of memory");
+	
+	//Save (A + I) in tmp
+	memcpy(tmp, src, n*n);
+	int i;
+	for (i = 0; i < n; i++) tmp[i*n + i] = 1;
+	
+	//Sanity check. We want this loop to run for clog2(n-1) times. Note that
+	//n is assumed to be 2 or greater at this point.
+	//Suppose n = 2: the loop doesn't run (correct)
+	//If n = 3, the loop runs once (correct)
+	//If n = 4, the loop runs twice (correct)
+	//If n = 5, the loop runs twice (correct)
+	//If n = 6, the loop runs three times (correct)
+	//Okay, I'm satisfied
+	int logerand = n - 2;
+	while (logerand > 0) {
+		adj_mat_mul(tmp2, tmp, tmp, n);
+		char *swap_tmp = tmp;
+		tmp = tmp2;
+		tmp2 = swap_tmp;
+		logerand >>= 1;
+		//puts("After squaring: ");
+		//print_adj_mat(tmp, n);
+	}
+	
+	adj_mat_mul(dest, src, tmp, n);
+	
+	free(tmp2);
+	free(tmp);
+}
+#else
+;
+#endif
+
+
+#endif
---
+++ heap.h
@@ -0,0 +1,164 @@
+#ifdef MM_IMPLEMENT
+	#ifndef HEAP_H_IMPLEMENTED
+		#define HEAP_H_IMPLEMENTED 1
+		#define SHOULD_INCLUDE 1
+	#else
+		#define SHOULD_INCLUDE 0
+	#endif
+#else
+	#ifndef HEAP_H
+		#define HEAP_H 1
+		#define SHOULD_INCLUDE 1
+	#else
+		#define SHOULD_INCLUDE 0
+	#endif
+#endif
+
+#if SHOULD_INCLUDE
+
+#ifdef MM_IMPLEMENT
+#undef MM_IMPLEMENT
+#include "heap.h"
+#define MM_IMPLEMENT 1
+#endif
+
+#include <string.h>
+
+#ifndef MM_IMPLEMENT
+#ifndef HAVE_COMPAR_TYPEDEF
+#define HAVE_COMPAR_TYPEDEF 1
+typedef int (*compar_fn)(void const *a, void const *b);
+#endif
+#endif
+
+#ifdef MM_IMPLEMENT
+//Implements a min-heap (specifically, the item at the root of the heap will
+//compare as being smaller or equal to all the others acording to the given 
+//comparison function [following the usual convention that a negative return 
+//means the first argument goes before the second]).
+//Assumes base[root] is empty, and fills it with the smallest of the following
+//three elements: {base[2*root + 1], base[2*root + 2], elem}. If a child element
+//was written, the function keeps bublling upwards into the space left behind
+//after the child element was moved. Of course, this function uses parameter n 
+//(the total size of the heap) to avoid going out of bounds. 
+static void bubble_upwards(
+	void *base, void const *elem, unsigned elem_sz, 
+	unsigned root, unsigned n, compar_fn *cmp) 
+{
+	//While the current node has at least two children...
+	//while (2*root + 2 < n)
+	while (root < (n-1)/2) {
+		unsigned left = 2*root + 1;
+		unsigned rite = 2*root + 2;
+		
+		unsigned smaller;
+		if (cmp(base + left*elem_sz, base + rite*elem_sz) <= 0) {
+			smaller = left;
+		} else {
+			smaller = rite;
+		}
+		
+		//If current element is less than or equal to smaller child,
+		//just write it and quit early.
+		//TODO: Should I do some benchmarking and change it to be 
+		//strictly less-than?
+		if (cmp(elem, base + smaller*elem_sz) <= 0) {
+			//This is where the given element goes. Write it and
+			//quit early
+			memcpy(base + root*elem_sz, elem, elem_sz);
+			return;
+		}
+		
+		//Move the child onto the current node
+		memcpy(base + root*elem_sz, base + smaller*elem_sz, elem_sz);
+		root = smaller;
+	}
+	
+	//Corner case: last node has only a left child
+	if (root*2 + 1 < n) {
+		unsigned left = root*2 + 1;
+		//Notice that the condition is the opposite of the one in the
+		//while loop just above us.
+		//If the current element is actually greater than the left child,
+		//copy hte left child onto the root and have the root point to 
+		//the space left behind after moving the left child.
+		if (cmp(elem, base + left*elem_sz) > 0) {
+			memcpy(base + root*elem_sz, base + left*elem_sz, elem_sz);
+			root = left;
+		}
+	}
+	
+	//root points to where the given element should be inserted
+	memcpy(base + root*elem_sz, elem, elem_sz);
+	return;
+}
+
+//Same semantics as bubble_upwards, but downwards
+static void bubble_downwards(
+	void *base, void const *elem, unsigned elem_sz,
+	unsigned root, unsigned n, compar_fn *cmp
+) {
+	while (root > 0) {
+		unsigned parent = (root - 1)/2;
+		
+		//If parent is less-or-equal to given element, break early
+		if (cmp(base + parent*elem_sz, elem, elem_sz) <= 0) {
+			break;
+		}
+		
+		memcpy(base + root*elem_sz, base + parent*elem_sz, elem_sz);
+		root = parent;
+	}
+	
+	//root points to where we should put the given element
+	//TODO: should I add a check that makes sure base+root*elem_sz != elem?
+	memcpy(base + root*elem_sz, elem, elem_sz);
+	return;
+}
+#endif
+
+//n should be the size of the heap INCLUDING the element we are about to add
+void __heap_insert(void *base, void const *elem, unsigned elem_sz, unsigned n, compar_fn *cmp)
+#ifdef MM_IMPLEMENT
+{
+	bubble_downwards(base, elem, elem_sz, n-1, n, cmp);
+}
+#else
+;
+#define heap_insert(h, e, n, cmp) \
+	__heap_insert(h, e, sizeof(*(h)), n, cmp)
+	
+#define vector_heap_insert(v, e, cmp)                    \
+do {                                                     \
+	vector_extend_if_full(v);                        \
+	__heap_insert(v, e, sizeof(*(v)), v##_len, cmp); \
+} while(0)
+#endif
+
+//n is the current size of the heap. The calling code must make sure to decrement
+//its own length variable
+void __heap_pop(void *base, void *elem_dest, unsigned elem_sz, unsigned n, compar_fn *cmp)
+#ifdef MM_IMPLEMENT
+{
+	//Copy the root element to the given destination
+	memcpy(elem_dest, base, elem_sz);
+	
+	//Bubble the last element of the heap downwards
+	//TODO: there must be a more efficient way to do this...
+	bubble_upwards(base, base + (n-1)*elem_sz, elem_sz, 0, n-1, cmp);
+}
+#else
+;
+#define heap_pop(h, e, n, cmp) \
+	__heap_pop(h, e, sizeof(*(h)), n, cmp)
+
+#define vector_heap_pop(v, e, cmp)                    \
+do {                                                  \
+	__heap_pop(v, e, sizeof(*(v)), v##_len, cmp); \
+	vector_pop(v);                                \
+while(0)
+#endif
+
+//TODO: heapify
+
+#endif
